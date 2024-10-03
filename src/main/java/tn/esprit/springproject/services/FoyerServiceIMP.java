86b09759a16c8cc75fa867729b9c36a557ca0b91
aziztheiri <alatheiri123@gmail.com>
2024-10-03 10:16:45 +0100

    x

---
+++ src/main/java/tn/esprit/springproject/services/FoyerServiceIMP.java
@@ -0,0 +1,32 @@
+package tn.esprit.springproject.services;
+
+import tn.esprit.springproject.entities.Foyer;
+
+import java.util.List;
+
+public class FoyerServiceIMP implements IFoyerService{
+    @Override
+    public Foyer addFoyer(Foyer foyer) {
+        return null;
+    }
+
+    @Override
+    public Foyer updateFoyer(Foyer foyer) {
+        return null;
+    }
+
+    @Override
+    public List<Foyer> retrieveAllFoyer() {
+        return null;
+    }
+
+    @Override
+    public Foyer retrieveFoyerById(Long idF) {
+        return null;
+    }
+
+    @Override
+    public void deleteFoyerById(Long idF) {
+
+    }
+}
---
+++ src/main/java/tn/esprit/springproject/services/IFoyerService.java
@@ -0,0 +1,13 @@
+package tn.esprit.springproject.services;
+
+import tn.esprit.springproject.entities.Foyer;
+
+import java.util.List;
+
+public interface IFoyerService {
+    public Foyer addFoyer(Foyer foyer);
+    public Foyer updateFoyer(Foyer foyer);
+    public List<Foyer> retrieveAllFoyer();
+    public Foyer retrieveFoyerById(Long idF);
+    public void deleteFoyerById(Long idF);
+}
