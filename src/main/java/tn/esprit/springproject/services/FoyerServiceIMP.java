a77e1ef8c5dc0af3f3578857356747bf55403656
aziztheiri <alatheiri123@gmail.com>
2024-10-03 10:33:25 +0100

    x

--- src/main/java/tn/esprit/springproject/services/FoyerServiceIMP.java
+++ src/main/java/tn/esprit/springproject/services/FoyerServiceIMP.java
@@ -1,32 +1,37 @@
 package tn.esprit.springproject.services;
 
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
 import tn.esprit.springproject.entities.Foyer;
+import tn.esprit.springproject.repositories.FoyerRepository;
 
 import java.util.List;
-
+@AllArgsConstructor
+@Service
 public class FoyerServiceIMP implements IFoyerService{
+    private FoyerRepository foyerRepository ;
     @Override
     public Foyer addFoyer(Foyer foyer) {
-        return null;
+        return foyerRepository.save(foyer);
     }
 
     @Override
     public Foyer updateFoyer(Foyer foyer) {
-        return null;
+        return foyerRepository.save(foyer);
     }
 
     @Override
     public List<Foyer> retrieveAllFoyer() {
-        return null;
+        return foyerRepository.findAll();
     }
 
     @Override
     public Foyer retrieveFoyerById(Long idF) {
-        return null;
+        return foyerRepository.findById(idF).orElse(null);
     }
 
     @Override
     public void deleteFoyerById(Long idF) {
-
+    foyerRepository.deleteById(idF);
     }
 }
--- src/main/java/tn/esprit/springproject/services/IFoyerService.java
+++ src/main/java/tn/esprit/springproject/services/IFoyerService.java
@@ -5,6 +5,7 @@ import tn.esprit.springproject.entities.Foyer;
 import java.util.List;
 
 public interface IFoyerService {
+
     public Foyer addFoyer(Foyer foyer);
     public Foyer updateFoyer(Foyer foyer);
     public List<Foyer> retrieveAllFoyer();
