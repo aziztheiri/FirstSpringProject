6bb0271ded6cb2439ae76a07e8f748f875264c13
aziztheiri <alatheiri123@gmail.com>
2024-10-03 11:28:02 +0100

    x

---
+++ src/main/java/tn/esprit/springproject/restControllers/FoyerRestController.java
@@ -0,0 +1,35 @@
+package tn.esprit.springproject.restControllers;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import tn.esprit.springproject.entities.Foyer;
+import tn.esprit.springproject.services.FoyerServiceIMP;
+import tn.esprit.springproject.services.IFoyerService;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+public class FoyerRestController {
+    private IFoyerService iFoyerService;
+    @PostMapping("/addFoyer")
+    public Foyer addFoyer(@RequestBody Foyer foyer) {
+        return iFoyerService.addFoyer(foyer);
+    }
+    @PutMapping("/updateFoyer")
+    public Foyer updateFoyer(@RequestBody Foyer foyer) {
+        return iFoyerService.updateFoyer(foyer);
+    }
+    @GetMapping("/getAllFoyer")
+    public List<Foyer> retrieveAllFoyer() {
+        return iFoyerService.retrieveAllFoyer();
+    }
+    @GetMapping("/findFoyerById/{idF}")
+    public Foyer retrieveFoyerById(@PathVariable Long idF) {
+        return iFoyerService.retrieveFoyerById(idF);
+    }
+    @DeleteMapping("deleteFoyerById/{idF}")
+    public void deleteFoyerById(@PathVariable Long idF) {
+         iFoyerService.deleteFoyerById(idF);
+    }
+}
