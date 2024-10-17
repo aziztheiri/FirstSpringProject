664a354f8b62745b61d89781cfb8a8bb956081dc
aziztheiri <alatheiri123@gmail.com>
2024-10-17 11:27:16 +0100

    x

--- src/main/java/tn/esprit/springproject/entities/Universite.java
+++ src/main/java/tn/esprit/springproject/entities/Universite.java
@@ -1,10 +1,18 @@
 package tn.esprit.springproject.entities;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 import java.io.Serializable;
 
 @Entity
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
 public class Universite implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
