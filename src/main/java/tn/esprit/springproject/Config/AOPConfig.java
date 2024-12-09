747f9035ba3d7f7868568a752bf870970c7552f1
aziztheiri <alatheiri123@gmail.com>
2024-12-09 16:31:59 +0100

    AOP

--- src/main/java/tn/esprit/springproject/Config/AOPConfig.java
+++ src/main/java/tn/esprit/springproject/Config/AOPConfig.java
@@ -2,7 +2,9 @@ package tn.esprit.springproject.Config;
 
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -21,5 +23,14 @@ public class AOPConfig {
         String name = joinPoint.getSignature().getName();
         log.info("Out  method " + name + " : ");
     }
+    @Around("execution(* tn.esprit.springproject.services.*.*(..))")
+    public Object profile(ProceedingJoinPoint pjp) throws Throwable {
+        long start = System.currentTimeMillis();
+        Object obj = pjp.proceed();
+        long elapsedTime = System.currentTimeMillis() - start;
+        log.info("Method execution time: " + elapsedTime + " milliseconds.");
+        return obj;
+    }
+
 
 }
